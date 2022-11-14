1ab76e7aea9e199fc7149cdcdedf351aad1a839c
PCTai <pntai121101@gmail.com>
2022-11-14 17:20:19 +0700

    Quiz app

---
+++ src/constants/questions.js
@@ -0,0 +1,28 @@
+
+
+export const questions =[
+    {
+        id: 1,
+        question: '1 + 1',
+        list: [1,4,2,3],
+        answer: 3,
+    },
+    {
+        id: 2,
+        question: '3 x 5',
+        list: [10,14,12,15],
+        answer: 15,
+    },
+    {
+        id: 3,
+        question: '10 / 2',
+        list: [5,4,2,3],
+        answer: 5,
+    },
+    {
+        id: 4,
+        question: '10 - 8',
+        list: [1,4,2,3],
+        answer: 2,
+    }
+]
--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import {questions} from '../../constants/questions'
 function Quiz() {
+    const [answers, setAnswers] = useState(questions[0].list);
+    const [index, setIndex] =useState(1);
+    console.log(answers);
+    const handleNext=() =>{
+        if(index< questions.length-1){
+            let newIndex= index+1;
+            setIndex(newIndex);
+            setAnswers(questions[index-1].list);
+        }
+    }
     return ( 
-        <h3>Quiz</h3>
+        <div className="quiz container w-full m-auto pt-28">
+            <h3 className="text-3xl text-blue-900 font-bold text-center"> Quiz app</h3>
+            <h3 className="text-xl font-bold mt-10">Question {index}: {questions[index].question} ?</h3>
+            <div className="answer mt-4">
+                {answers.map(item =>(
+                    <div key={item} className="itemAnswer p-2 bg-white mb-4 cursor-pointer">{item}</div>
+                ))}
+            </div>
+            <button onClick={handleNext} className="rounded-lg pt-2 pb-2 pr-6 pl-6 bg-blue-900 text-white">Next</button>
+            <button disabled={index===questions.length-1?false: true} className={`${index<questions.length-1?'opacity-10' :''} block mt-10 rounded-lg pt-2 pb-2 pr-6 pl-6 bg-blue-900 text-white`}>Submit</button>
+        </div>
      );
 }
 
