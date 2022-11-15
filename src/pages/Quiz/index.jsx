afbbc7d07058abdd4b5edbab8f4285d02751e9fb
PCTai <pntai121101@gmail.com>
2022-11-15 16:37:41 +0700

    fix quiz and filter

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -7,28 +7,43 @@ function Quiz() {
     const length= questions.length;
     const [answers, setAnswers] = useState(questions[0].list);
     const [index, setIndex] =useState(1);
+    const [score, setScore] =useState(0);
     const [active, setActive] =useState(0);
     const [results, SetResult] = useState(Array(length));
+    const [open, setOpen] = useState(false);
     // console.log(results);
     const handleNext=() =>{
         if(index<= questions.length-1){
             let newIndex= index+1;
             setIndex(newIndex);
             setAnswers(questions[newIndex-1].list);
+            setActive(0);
         }
-        setActive(0);
+        
     }
     const handlePrev=() =>{
-        if(index<=1){
+        
+        let newIndex=index-1;
+        if(newIndex<1){
             setIndex(1);
             setAnswers(questions[0].list);
         }
         else{
-            let newIndex= index-1;
+            
+            const newAnswers= questions[newIndex-1].list;
             setIndex(newIndex)
-            setAnswers(questions[newIndex-1].list);
+            setAnswers(newAnswers);
+            // console.log(active);
+            
+            newAnswers.forEach((item,index) =>{
+                results.forEach((result) =>{
+                    if(result===item){
+                        setActive(index+1);
+                    }
+                })      
+            })
         }
-        setActive(0);
+        
     }
     const handleChooseAnswer= (value,i) =>{
         const newResult= results;
@@ -50,12 +65,23 @@ function Quiz() {
                 score+=1;
             }
             // console.log(score);
-        })
+        })  
         // console.log(`Diem so cua ban la : ${score}/${length}`);
-        alert(`Your score :  ${score}/${length}`)
+        // alert(`Your score :  ${score}/${length}`)
+        setScore(score);
+        setOpen(true);
     }
     return ( 
         <div className="quiz container w-full m-auto pt-28 ">
+            {open && 
+                <div
+                    
+                className="model fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-center items-center">
+                    <div className="model-container w-96 h-80 flex justify-center flex-col items-center bg-white shadow-md">
+                        <h3 className="text-2xl">Your score : {score}</h3>
+                        <button onClick={() =>setOpen(false)} className="mt-8 pl-10 pr-10 bg-blue-900 text-white pt-4 pb-4 rounded-xl">OK</button>
+                    </div>
+                </div>}
             <Link to='/' className=" block text-center">{'<-'}Back home</Link>
             <h3 className="text-3xl text-blue-900 font-bold text-center"> Quiz app</h3>
             
